05bcc347462fe9f2ea471d4e0fbfe77f9ce21b52
Sounds Dope Audio <incorporatedRx@gmail.com>
2025-12-08 13:40:22 -0800

    Delete Source directory

--- Source/MidiExporter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MidiGenerator.h"
-#include <juce_audio_basics/juce_audio_basics.h>
-
-// Defaults only on the declaration.
-void exportMixBundleToMidiFile(const MidiGenerator::MixBundle& bundle,
-    juce::MidiFile& outFile,
-    int ppq = 960,
-    int midiChannel = 1);
